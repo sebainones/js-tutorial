85a3210eb91fe49b615168509464fa7305f1c918
sebainones <sebainones@gmail.com>
2016-06-13 18:38:18 +0200

    Some variables and comments added.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+// This is a single line comment.
 var name = prompt("Ingresa tu nombre");
 alert("Hola "+ name);
 
-var year;
+var myVariable = 200;
+alert(myVariable);
+
+myVariable = "String";
+alert(myVariable);
+
+
+var aVariable;
+aVariable = 5;
